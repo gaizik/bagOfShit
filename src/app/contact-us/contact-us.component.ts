cc8becc3ccd45f7c8e6bbdc0b9c61cc1f988e933
gaizik <gaizik1@gmail.com>
2019-01-30 08:13:33 +0200

    go

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -9,12 +9,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ContactUsComponent implements OnInit {
   contactUsForm: FormGroup;
   submitted = false;
-  constructor(private formbuilder: FormBuilder) { }
+  constructor(private formbuilder: FormBuilder ) { }
 
   ngOnInit() {
     this.contactUsForm = this.formbuilder.group({
-      fullName: ['', Validators.required],
-      email: ['', Validators.required, Validators.email]
+      fullName: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, Validators.email]],
+      message:['', [Validators.required, Validators.maxLength(250), Validators.minLength(5)]]
     });
   }
 
@@ -23,6 +24,7 @@ export class ContactUsComponent implements OnInit {
   onSubmit() {
     this.submitted = true;
     if (this.contactUsForm.invalid) {
+      console.log(this.contactUsForm.invalid)
       return;
     }
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.contactUsForm.value));
